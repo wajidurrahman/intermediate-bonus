c35d065d5a6bd593d877ae3ba8b7d1183df98138
Wajidur Rahman <wrfuad0@gmail.com>
2021-04-26 13:13:13 +0600

    How to implement Facebook Like button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 
 function App() {
+  const [likeColor, setLikeColor] = useState('');
+  const handleLike = () => {
+    const color = likeColor ? '' : 'primary';
+    setLikeColor(color);
+  }
   return (
     <div className="App">
+      <AccessAlarmIcon/>
+      <ThumbUpIcon onClick={handleLike} color={likeColor}/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
